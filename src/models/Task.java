9dae684a49ed3b15a2006b239442252a1a5e03ed
inon25 <inon0718@gmail.com>
2021-04-16 09:24:01 +0900

    Add getAllTasks

--- src/models/Task.java
+++ src/models/Task.java
@@ -13,11 +13,10 @@ import javax.persistence.Table;
 
 @Entity
 @NamedQueries({
-    @NamedQuery(
-        name = "getAllTasks",
-        query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
-    )
+        @NamedQuery(name = "getAllTasks", query = "SELECT t FROM Task AS t ORDER BY t.id DESC"),
+        @NamedQuery(name = "getTasksCount", query = "SELECT COUNT(t) FROM Task AS t")
 })
+
 @Table(name = "tasks")
 public class Task {
     @Id
